4f5a1c00d5ab0bcae78c2d530930364c4807dab1
felixesteban100 <felixesteban100@gmail.com>
2023-06-07 06:27:04 -0400

    v2 ok

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,8 +6,8 @@ import {
   type DefaultSession,
 } from "next-auth";
 // import DiscordProvider from "next-auth/providers/discord";
-// import GoogleProvider from "next-auth/providers/google";
-// import { env } from "~/env.mjs";
+import GoogleProvider from "next-auth/providers/google";
+import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
 /**
@@ -53,10 +53,10 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }), */
 
-    /* GoogleProvider({
+    GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET, 
-    }) */
+    })
 
     /**
      * ...add more providers here.
