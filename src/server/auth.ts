28898b7a51af4283385bbaee9eb17d44f49cd0d1
felixesteban100 <felixesteban100@gmail.com>
2023-06-06 20:42:58 -0400

    v1 ok

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,8 +6,8 @@ import {
   type DefaultSession,
 } from "next-auth";
 // import DiscordProvider from "next-auth/providers/discord";
-import GoogleProvider from "next-auth/providers/google";
-import { env } from "~/env.mjs";
+// import GoogleProvider from "next-auth/providers/google";
+// import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
 /**
